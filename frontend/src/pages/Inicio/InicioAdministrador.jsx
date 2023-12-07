2c7bcafea8d5cfbccb6388dff16343b2a1caaabe
primuri <priscila@primarix.com>
2023-12-06 22:36:53 -0600

    Preguntas frecuentes terminado

--- frontend/src/pages/Inicio/InicioAdministrador.jsx
+++ frontend/src/pages/Inicio/InicioAdministrador.jsx
@@ -1,6 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import inicio from '../../assets/inicio.jpg';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import { createPopper } from '@popperjs/core';
+
 export const InicioAdministrador = ({ usuario }) => {
 
   const usuarioObjeto = JSON.parse(usuario);
@@ -45,8 +49,8 @@ export const InicioAdministrador = ({ usuario }) => {
       <div className="d-flex p-5 mt-5 justify-content-center align-items-center">
         <div className="col">
             <div className="row align-items-center">
-                <div className="col-6 mx-auto text-center">
-                <h1 className="mt-5">¡Bienvenido(a), {obtenerNombreDeUsuario(correo)}!</h1>
+                <div className="col-6 mx-auto">
+                <h1 className="mt-5 text-center">¡Bienvenido(a), {obtenerNombreDeUsuario(correo)}!</h1>
                 <br/>
                 <p className="fs-5 text-justify m-5 mt-0 mb-4">
                     <b>SIAP</b>, una solución para la Administración de Proyectos en el Centro de Investigación en Matemática Pura y Aplicada de la Universidad de Costa Rica.  Ha sido diseñado para satisfacer las necesidades específicas del CIMPA y a su vez ofrecer una plataforma centralizada, ordenada y segura para potenciar la eficiencia de cada proyecto.
@@ -63,15 +67,15 @@ export const InicioAdministrador = ({ usuario }) => {
             <div className="row mt-4">
                 <div className="col-6 m-5">   
                     <h4 className="pb-3">Preguntas frecuentes</h4>
-                    <div className="accordion">
+                    <div className="accordion" id="accordionExample"> 
 
                       <div className="accordion-item">
-                              <h2 className="accordion-header" id="headingTwo">
-                                <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                              <h2 className="accordion-header" id="headingOne">
+                                <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
                                     ¿Cómo se agrega un nuevo registro?
                                 </button>
                               </h2>
-                              <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+                              <div id="collapseOne" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
                                 <div className="accordion-body">
                                 Haga click en el botón "Agregar" ubicado en la esquina superior izquierda. Esto abrirá un formulario para ingresar la información necesaria. Complete el formulario proporcionando la información solicitada. Una vez que haya ingresado todos los detalles, haga clic en el botón "Agregar" dentro del formulario para guardar el nuevo registro. En caso de ya no querer agregarlo, dele click a la "x" en la esquina superior derecha.
                                 </div>
@@ -92,15 +96,13 @@ export const InicioAdministrador = ({ usuario }) => {
                             </div>
                       </div>
 
-
-                      
                       <div className="accordion-item">
-                            <h2 className="accordion-header" id="headingTwo">
-                              <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                            <h2 className="accordion-header" id="headingThree">
+                              <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
                                   ¿Cómo se elimina un registro?
                               </button>
                             </h2>
-                            <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+                            <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
                               <div className="accordion-body">
                                Haga clic en el registro que desea, esto abrirá un formulario. De click en el botón "Eliminar" ubicado en la parte inferior del formulario. En caso de ya no querer eliminarlo, dele click a la "x" en la esquina superior derecha.
                               </div>
@@ -108,12 +110,12 @@ export const InicioAdministrador = ({ usuario }) => {
                     </div>
 
                         <div className="accordion-item">
-                            <h2 className="accordion-header" id="headingTwo">
-                              <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                            <h2 className="accordion-header" id="headingFour">
+                              <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
                                   ¿Cómo se agrega un proyecto?
                               </button>
                             </h2>
-                            <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+                            <div id="collapseFour" class="accordion-collapse collapse" aria-labelledby="headingFour" data-bs-parent="#accordionExample">
                               <div className="accordion-body">
                               Para que un proyecto adquiera existencia formal, es necesario que previamente exista una propuesta correspondiente. La transición de una propuesta a proyecto se materializa únicamente mediante la aprobación de dicha propuesta. Por lo tanto, para agregar un nuevo proyecto, el proceso implica dirigirse a la sección de "Propuestas de Proyectos", donde la aprobación de la propuesta es el paso clave. Una vez aprobada, la propuesta se transformará en un proyecto, visible y gestionable en la sección designada para "Proyectos". Este flujo asegura un proceso estructurado y controlado en la evolución de las ideas propuestas hacia la ejecución efectiva de proyectos.
                               </div>
@@ -121,12 +123,12 @@ export const InicioAdministrador = ({ usuario }) => {
                         </div>
 
                         <div className="accordion-item">
-                            <h2 className="accordion-header" id="headingThree">
-                              <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                            <h2 className="accordion-header" id="headingFive">
+                              <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
                                   ¿Por qué no se puede editar o borrar un proyecto?
                               </button>
                             </h2>
-                            <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+                            <div id="collapseFive" class="accordion-collapse collapse" aria-labelledby="headingFive" data-bs-parent="#accordionExample">
                               <div class="accordion-body">
                               Cada proyecto tiene una o varias versiones. Es por esto que si se desea editar, debe de ser directamente en una de las versiones y no en un proyecto en general como tal. Por otro lado, por seguridad, estos datos no pueden ser eliminados ya que es informacion sensible. 
                               </div>
@@ -134,25 +136,25 @@ export const InicioAdministrador = ({ usuario }) => {
                         </div>
 
                         <div className="accordion-item">
-                            <h2 className="accordion-header" id="headingThree">
-                              <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                            <h2 className="accordion-header" id="headingSix">
+                              <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseSix" aria-expanded="false" aria-controls="collapseSix">
                                   ¿Por qué no se puede agregar más de un presupuesto?
                               </button>
                             </h2>
-                            <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+                            <div id="collapseSix" class="accordion-collapse collapse" aria-labelledby="headingSix" data-bs-parent="#accordionExample">
                               <div class="accordion-body">
-                              txt
+                              Un proyecto tiene únicamente un presupuesto, no se deben ingresar varios. En caso de desear modificarlo, se puede hacer.
                               </div>
                             </div>
                         </div>
 
                         <div className="accordion-item">
-                            <h2 className="accordion-header" id="headingThree">
-                            <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                            <h2 className="accordion-header" id="headingSeven">
+                            <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseSeven" aria-expanded="false" aria-controls="collapseSeven">
                                 ¿Cómo saber cuando un dato es opcional?
                             </button>
                             </h2>
-                            <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+                            <div id="collapseSeven" class="accordion-collapse collapse" aria-labelledby="headingSeven" data-bs-parent="#accordionExample">
                               <div class="accordion-body">
                               En cada formulario, ya sea para agregar o editar un registro, si hay algún dato que no es requerido se detalla explícitamente que es opcional al lado de su nombre. 
                               </div>
