e19039255f6d03cb4edb48c6be7a4bb9f1a143a4
Gorki Ivan Romero Valerio <61533709+Gorkir6@users.noreply.github.com>
2024-03-15 15:00:38 -0600

    cambios en version de presupuesto. Eliminar saldo.

--- frontend/src/pages/GestionPresupuestos/utils.js
+++ frontend/src/pages/GestionPresupuestos/utils.js
@@ -1,4 +1,4 @@
-export const columnsVI = ['ID', 'Version', 'Monto','Saldo', 'Detalle', 'Partidas']
-export const dataKeyVI = ['id_version_presupuesto', 'version', 'monto', 'saldo', 'detalle', 'Partidas']
+export const columnsVI = ['ID', 'Version', 'Monto', 'Detalle', 'Partidas']
+export const dataKeyVI = ['id_version_presupuesto', 'version', 'monto', 'detalle', 'Partidas']
 export const columnsPartidas = ['ID','Monto','Saldo', 'Detalle']
 export const dataKeyPartidas = ['id_partida','monto', 'saldo', 'detalle']
