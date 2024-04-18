ea0783c0970740a02364d7d31dfd2ef19ff7f658
Ariel <arielgrandasolano@gmail.com>
2024-04-18 13:44:15 -0600

    Maquetización y css listo en reportes de PDF

--- frontend/src/utils/ReportButton.jsx
+++ frontend/src/utils/ReportButton.jsx
@@ -1,13 +1,14 @@
 import { useState, useEffect } from "react";
 import PropTypes from 'prop-types';
 import reporte from '../assets/reporte.png'
+import { ReportHeader } from "../components/Layout/ReportHeader";
 
-export const ReportButton = ({tableData, reportTittle, colNames, dataKeys}) => {
+export const ReportButton = ({ tableData, reportTittle, colNames, dataKeys }) => {
     const [selectedOption, setSelectedOption] = useState("PDF");
     const [buttonClicked, setButtonClicked] = useState(false);
 
     const handleButtonClick = () => {
-        if(buttonClicked){
+        if (buttonClicked) {
             console.log("ADELANTE!");
         } else {
             console.log("No se tocó botón");
@@ -23,10 +24,10 @@ export const ReportButton = ({tableData, reportTittle, colNames, dataKeys}) => {
     };
 
     return (
-        <div style={{marginRight: '1vw'}}>
-            <button type="button" className="background-color-celeste-ucr border-0 text-light rounded-3 p-2 d-flex" onClick={handleButtonClick}> 
-                <span className='icono'><img width={"20px"} src={reporte}/></span> 
-                <select className="rounded-start-3 selectPersonalized form-select d-inline p-2 shadow-sm" onBlur={handleBlur} onFocus={handleFocus} onChange={(event) => setSelectedOption(event.target.value)} value={selectedOption} style={{ fontSize: '12px', lineHeight: '80%', width: '3.5rem', backgroundPosition: 'right 0rem center'}}> 
+        <div style={{ marginRight: '1vw' }}>
+            <button type="button" className="background-color-celeste-ucr border-0 text-light rounded-3 p-2 d-flex" onClick={handleButtonClick}>
+                <span className='icono'><img width={"20px"} src={reporte} /></span>
+                <select className="rounded-start-3 selectPersonalized form-select d-inline p-2 shadow-sm" onBlur={handleBlur} onFocus={handleFocus} onChange={(event) => setSelectedOption(event.target.value)} value={selectedOption} style={{ fontSize: '12px', lineHeight: '80%', width: '3.5rem', backgroundPosition: 'right 0rem center' }}>
                     <option>PDF</option>
                     <option>EXCEL</option>
                     <option>CALC</option>
@@ -44,8 +45,18 @@ export const ReportButton = ({tableData, reportTittle, colNames, dataKeys}) => {
 
 // Función para obtener el valor de un objeto dado un camino (path) de claves
 function getValueByPath(obj, path) {
-    return path.split('.').reduce((acc, part) => acc && acc[part], obj);
+    const value = path.split('.').reduce((acc, part) => acc && acc[part], obj);
+
+    // Verificar si el valor obtenido es una fecha en el formato proporcionado
+    const fechaRegex = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}Z$/;
+    if (typeof value === 'string' && fechaRegex.test(value)) {
+        // Extraer solo la parte de la fecha (YYYY-MM-DD) del valor
+        return value.split('T')[0];
+    }
+
+    return value;
 }
+
 
 import React from 'react';
 
@@ -56,7 +67,61 @@ function getLength(value) {
 
 
 export const ReportePDF = () => {
-    var tableData = {
+    var tableData = [{
+        "id_codigo_vi": "1-2024",
+        "id_codigo_cimpa_fk": {
+            "id_codigo_cimpa": "1-2024",
+            "id_colaborador_principal_fk": {
+                "id_colaborador_principal": 7,
+                "id_vigencia_fk": {
+                    "id_vigencia": 63,
+                    "fecha_inicio": null,
+                    "fecha_fin": null
+                },
+                "id_academico_fk": {
+                    "id_academico": 18,
+                    "id_nombre_completo_fk": {
+                        "id_nombre_completo": 83,
+                        "nombre": "Jorge",
+                        "apellido": "Poltronieri",
+                        "segundo_apellido": "Vargas"
+                    },
+                    "id_area_especialidad_fk": {
+                        "id_area_especialidad": 37,
+                        "nombre": "Estadística"
+                    },
+                    "id_area_especialidad_secundaria_fk": {
+                        "id_area_especialidad": 38,
+                        "nombre": ""
+                    },
+                    "universidad_fk": {
+                        "id_universidad": 18,
+                        "pais": "Costa Rica",
+                        "nombre": "UCR"
+                    },
+                    "cedula": "104051422",
+                    "foto": null,
+                    "sitio_web": "",
+                    "grado_maximo": "Dr",
+                    "correo": "JORGE.POLTRONIERI@ucr.ac.cr",
+                    "correo_secundario": "",
+                    "unidad_base": "Estadística",
+                    "categoria_en_regimen": "Catedrático",
+                    "pais_procedencia": "Costa Rica"
+                },
+                "tipo": "Principal",
+                "carga": "1/2",
+                "estado": "Activo"
+            },
+            "objetivo_general": "",
+            "estado": "Aprobada",
+            "nombre": "Proyecto de manufactura",
+            "descripcion": "lorem ipsumipsumisumipsumipsumipsumsumipsumipsumipsumsumipsumipsumipsumsumipsumipsumipsumpsumipsumipsum lorem ipsumipsumipsumipsumipsumpsums ipsumipsumipsumipsumipsumpsums sssssassssssas dasda  sssssss as adas  asdas d asd as dasda asdasdad   asdasda sd sda as dasdad daasassipsum lorem ipsum lorem m lorem ipsumlorem ipsum lorem ipsum lorem ipsum lorem ipsum",
+            "fecha_vigencia": "2023-03-01T00:00:00Z",
+            "actividad": "Investigación actvia"
+        }
+    },
+    {
         "id_codigo_vi": "1-2024",
         "id_codigo_cimpa_fk": {
             "id_codigo_cimpa": "1-2024",
@@ -110,12 +175,15 @@ export const ReportePDF = () => {
             "actividad": "Investigación actvia"
         }
     }
+    ]
 
-    var reportTitle = "Proyectos";
+    var reportTitle = "Proyecto";
 
     var colNames = ['Codigo VI', 'Código CIMPA', 'Vigencia', 'Nombre', 'Estado', 'Descripción', 'Actividad'];
 
     var dataKeys = ['id_codigo_vi', 'id_codigo_cimpa_fk.id_codigo_cimpa', 'id_codigo_cimpa_fk.fecha_vigencia', 'id_codigo_cimpa_fk.nombre', 'id_codigo_cimpa_fk.estado', 'id_codigo_cimpa_fk.descripcion', 'id_codigo_cimpa_fk.actividad'];
+
+    var idKey = "id_codigo_vi"
 
     // Función para ajustar la altura de las tarjetas dentro de una fila
     const ajustarAlturaTarjetas = () => {
@@ -144,20 +212,29 @@ export const ReportePDF = () => {
     }, []);
 
     return (
-        <div className="container" style={{width: "700px"}}>
-            <h1>{reportTitle}</h1>
-            <div className="row">
-                {colNames.map((colName, index) => (
-                    <div key={index} className={`col-md-4 mb-4 ${getLength(getValueByPath(tableData, dataKeys[index])) > 300 ? 'col-md-12' : getLength(getValueByPath(tableData, dataKeys[index])) > 40 ? 'col-md-8' : getLength(getValueByPath(tableData, dataKeys[index])) > 10 ? 'col-md-4' : 'col-md-2'} flex-grow-1`}>
-                        <div className="card d-flex flex-column h-100 border-0 container-fluid" >
-                            <div className="card-body item-padding">
-                                <h5 className="card-title">{colName}:</h5>
-                                <p className="card-text flex-grow-1 text-break">{getValueByPath(tableData, dataKeys[index])}</p>
+        <div className="reporteGeneral">
+        <ReportHeader nombreReporte={reportTitle}/>
+        
+        <div className="container" style={{ width: "700px" }}>
+            {tableData.map((data) => (
+                <div className="row page-report">
+                    <div className="row">
+                        <h5 className="nombrePage">{reportTitle + " " + getValueByPath(data, idKey)}</h5>
+                    </div>
+                    {colNames.map((colName, index) => (
+                        <div key={index} className={`col-md-4 mb-4 ${getLength(getValueByPath(data, dataKeys[index])) > 300 ? 'col-md-12' : getLength(getValueByPath(data, dataKeys[index])) > 40 ? 'col-md-8' : getLength(getValueByPath(data, dataKeys[index])) > 10 ? 'col-md-4' : 'col-md-2'} flex-grow-1`}>
+                            <div className="card d-flex flex-column h-100 border-0 container-fluid elemento-reporte" >
+                                <div className="card-body item-padding">
+                                    <h6 className="card-title">{colName}:</h6>
+                                    <p className="card-text flex-grow-1 text-break">{getValueByPath(data, dataKeys[index])}</p>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                ))}
-            </div>
+                    ))}
+                </div>
+            ))}
+
+        </div>
         </div>
     );
 };
