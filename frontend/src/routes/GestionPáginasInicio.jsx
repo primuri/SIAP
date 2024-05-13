c7a109e107fb91208d94b80e446675a12d16f3f5
primuri <priscila@primarix.com>
2024-05-13 13:51:22 -0600

    GestionPáginasInicio.jsx

---
+++ frontend/src/routes/GestionPáginasInicio.jsx
@@ -0,0 +1,14 @@
+import { Route } from "react-router-dom"
+import {InicioAdministrador} from "../pages/Inicio/InicioAdministrador"
+import {InicioEvaluador} from "../pages/Inicio/InicioEvaluador"
+import {InicioInvestigador} from "../pages/Inicio/InicioInvestigador"
+import {InicioInvestigadorEvaluador} from "../pages/Inicio/InicioInvestigadorEvaluador"
+import {InicioInvitado} from "../pages/Inicio/InicioInvitado"
+
+export default [
+    <Route path='/inicio-administrador' element={<InicioAdministrador usuario={localStorage.getItem('user')} />}></Route>,
+    <Route path='/inicio-invitado' element={<InicioInvitado usuario={localStorage.getItem('user')} />}></Route>,
+    <Route path='/inicio-evaluador'  element={<InicioEvaluador usuario={localStorage.getItem('user')} />}></Route>,
+    <Route path='/inicio-investigador' element={<InicioInvestigador usuario={localStorage.getItem('user')} />}></Route>,
+    <Route path='/inicio-investigador-evaluador' element={<InicioInvestigadorEvaluador></InicioInvestigadorEvaluador>}></Route>
+]
